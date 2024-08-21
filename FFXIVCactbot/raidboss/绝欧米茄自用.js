55440c961180f0c3b1cbccb7ab24dbbb8d1d9ef3
Shinaria <mikusecrets@gmail.com>
2024-08-21 23:15:57 +0800

    更新绝欧笔记+触发器

--- FFXIVCactbot/raidboss/绝欧米茄自用.js
+++ FFXIVCactbot/raidboss/绝欧米茄自用.js
@@ -4,6 +4,7 @@ Options.Triggers.push({
     timeline: [
         '77.3 "抗死->大功率波动炮P" duration 4',
         '89.3 "昏乱->大功率波动炮P" duration 4',
+        '308.8 "抗死->能量投射+宇宙记忆" duration 0.9',
     ],
-    triggers: [],
+    triggers:[],
   });
